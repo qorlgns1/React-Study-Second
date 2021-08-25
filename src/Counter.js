a4d9925c6e899b39c252e320daec7a05d3fa9cb9
qorlgns1 <rlgns0610@gmail.com>
2021-08-25 20:56:16 +0900

    Hooks

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+    </div>
+  );
+};
